ecbcb2370bcc141ab56a3fa4261c6cf42e21ffb0
Tanner Muldoon <tannermuldoon@gatech.edu>
2018-12-11 16:35:28 -0500

    Detector with thread, but logic is not implemented

--- include/detector.h
+++ include/detector.h
@@ -3,6 +3,13 @@
 
 #include "units.h"
 
+#include <memory>
+
+#include <atomic>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+
 class Detector
 {
 public:
@@ -11,8 +18,29 @@ public:
 
     // Returns true if there is fire
     // Writes the distance and angle into the provided vars
-    bool checkForFire(AngleDeg* angle = nullptr, TempF* temp = nullptr);
+    bool checkForFire(AngleDeg* angle = nullptr, float* dist = nullptr);
 
     // Same as check for fire, but blocks until there is/isn't fire
-    bool waitForFire(AngleDeg* angle = nullptr, TempF* temp = nullptr, bool look = true);
+    bool waitForFire(AngleDeg* angle = nullptr, float* dist = nullptr, bool look = true);
+
+private:
+    std::atomic<bool> done;
+
+    std::unique_ptr<std::thread> detectThread;
+
+    bool found;
+    AngleDeg foundAngle;
+    float foundDist;
+
+    std::mutex foundMutex;
+    std::condition_variable condFound;
+    std::condition_variable condLost;
+
+private:
+    void detect();
+
+    void find(AngleDeg _foundAngle, float _foundDist);
+    void lose();
+
+    friend void detectorThread(Detector* det);
 };
--- src/detector.cpp
+++ src/detector.cpp
@@ -1,23 +1,101 @@
 
 #include "detector.h"
 
-Detector::Detector()
-{
+void detectorThread(Detector* det);
 
+Detector::Detector():
+    done(false),
+    detectThread(nullptr)
+{
+    detectThread = std::unique_ptr<std::thread>(new std::thread(detectorThread, this));
 }
 
 Detector::~Detector()
 {
-
+    done.store(true);
+    detectThread->join();
 }
 
-bool Detector::checkForFire(AngleDeg* angle, TempF* temp)
+bool Detector::checkForFire(AngleDeg* angle, float* dist)
 {
-    return false;
+    std::unique_lock<std::mutex> lk(foundMutex);
+
+    if (angle)
+        *angle = foundAngle;
+
+    if (dist)
+        *dist = foundDist;
+
+    bool check = found;
+
+    lk.unlock();
+
+    return check;
 }
 
-bool Detector::waitForFire(AngleDeg* angle, TempF* temp, bool look)
+bool Detector::waitForFire(AngleDeg* angle, float* dist, bool look)
 {
-    // block here
-    return checkForFire(angle);
+    std::unique_lock<std::mutex> lk(foundMutex);
+
+    if (look)
+    {
+        condFound.wait(lk, []{return found;});
+    }
+    else
+    {
+        condLost.wait(lk, []{return !found;});
+    }
+
+    std::unique_lock<std::mutex> lk(foundMutex);
+
+    if (angle)
+        *angle = foundAngle;
+
+    if (dist)
+        *dist = foundDist;
+
+    bool check = found;
+
+    lk.unlock();
+
+    return check;
+}
+
+void Detector::detect()
+{
+    // check the camera for fire
+    // call find() if it is found
+    // call lose() if it not found
+}
+
+void Detector::find(AngleDeg _foundAngle, float _foundDist)
+{
+    std::unique_lock<std::mutex> lk(foundMutex);
+
+    found = true;
+    foundAngle = _foundAngle;
+    foundDist = _foundDist;
+
+    condFound.notify_all();
+
+    lk.unlock();
+}
+
+void Detector::lose()
+{
+    std::unique_lock<std::mutex> lk(foundMutex);
+
+    found = false;
+
+    condLost.notify_all();
+
+    lk.unlock();
+}
+
+void detectorThread(Detector* det)
+{
+    while (!det.done.load())
+    {
+        det.detect();
+    }
 }
